8299eb925a049eba82f754d0592d9df3725e3273
logan fisher <loganfisher@logans-MacBook-Pro.local>
2020-03-31 16:42:03 -0500

    UPdated action

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const exec = require("@actions/exec")
 
-return exec.exec('DEBUG=release-it:*  release-it patch --preRelease=qa --ci --no-npm');
+return exec.exec('npm install && DEBUG=release-it:*  release-it patch --preRelease=qa --ci --no-npm');
