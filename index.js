453530a377ced3150e22137f6307de46bae4e977
logan fisher <loganfisher@logans-MacBook-Pro.local>
2020-03-31 20:55:25 -0500

    updated more

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const exec = require("@actions/exec")
 
-return exec.exec('npm start')
+return exec.exec('npm install --global release-it')
     .then(() => {
-        process.exit(0)
+        exec.exec('release-it patch --preRelease=qa --ci --no-npm')
+            .then(() => {
+                process.exit(0)
+            })
+            .catch((err) => {
+                console.error(err)
+                process.exit(1)
+            })
     })
-    .catch((err) => {
-        console.error(err)
-        process.exit(1)
-    })
+    
