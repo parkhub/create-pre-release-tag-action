48d07f3fbde824751381100df685d08762c25a4a
logan fisher <loganfisher@logans-MacBook-Pro.local>
2020-04-01 09:27:18 -0500

    Added remote

--- run.sh
+++ run.sh
@@ -11,6 +11,9 @@ git config --global url."https://${USERNAME}:${TOKEN}@github.com".insteadOf "htt
 echo "Cloning repo ${REPO}"
 git clone https://github.com/parkhub/${REPO}.git
 
+echo "Adding remote for repo ${REPO}"
+git remote add origin https://github.com/parkhub/${REPO}.git
+
 echo "Checking out QA branch"
 git checkout origin/qa
 
