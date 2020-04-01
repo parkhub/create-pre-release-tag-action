29c30394a286b3ef21a8aab2b53a4d1bf8cf70e0
logan fisher <loganfisher@logans-MacBook-Pro.local>
2020-04-01 09:22:03 -0500

    updated run file

--- run.sh
+++ run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+echo "Environment Variables"
+echo "REPO: ${REPO}"
+echo "USERNAME: ${USERNAME}"
+echo "TOKEN: ${TOKEN}"
+
+echo "Configuring git"
+git config --global url."https://${USERNAME}:${TOKEN}@github.com".insteadOf "https://github.com"
+
+echo "Cloning repo ${REPO}"
+git clone https://github.com/parkhub/${REPO}.git
+
+echo "Checking out QA branch"
+git checkout origin/qa
+
+echo "Running release-it"
 # $1 is the release type major, minor or patch
 DEBUG=release-it:* release-it patch --preRelease=qa --ci --no-npm
